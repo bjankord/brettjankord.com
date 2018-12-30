185eb9538075384f5153683e9ad950e8165cbab7
Brett Jankord <brett.jankord@cerner.com>
2018-12-30 06:25:43 -0600

    Try to fix styles not loading on blog posts

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,2 +1,3 @@
 // gatsby-browser.js
+require("./src/assets/styles.css")
 require("./src/assets/syntax-highlighter-theme.css")
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,6 @@ import get from 'lodash/get'
 import Helmet from 'react-helmet'
 
 import Layout from '../components/Layout'
-import '../assets/styles.css';
 
 class BlogIndex extends React.Component {
   render() {
