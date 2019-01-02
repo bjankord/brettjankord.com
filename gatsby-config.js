09ab1a4ad6527175c2692fccd637ecb14c4de22b
Brett Jankord <brett.jankord@cerner.com>
2019-01-02 04:39:49 -0600

    Add google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -49,12 +49,12 @@ module.exports = {
     },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
-    // {
-    //   resolve: `gatsby-plugin-google-analytics`,
-    //   options: {
-    //     //trackingId: `ADD YOUR TRACKING ID HERE`,
-    //   },
-    // },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-3084536-1`,
+      },
+    },
     {
       resolve: 'gatsby-plugin-feed',
       options: {
