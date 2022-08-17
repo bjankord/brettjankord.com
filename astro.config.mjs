415bab3d09a41d293c8663283290b8dde98b3bd2
Brett <bjankord@gmail.com>
2022-08-16 22:10:08 -0500

    Add initial tagging system

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,19 @@ import { defineConfig } from 'astro/config';
 import image from '@astrojs/image';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
 
 // https://astro.build/config
 export default defineConfig({
+  resolve: {
+      alias: {
+        '$': path.resolve(__dirname, './src'),
+      },
+  },
 	site: 'https://www.brettjankord.com',
 	integrations: [image(), mdx(), sitemap()],
 });
