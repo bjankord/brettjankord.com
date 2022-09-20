752490c223da240308445e993eb2a203aaa33038
Brett <bjankord@gmail.com>
2022-09-20 04:36:04 -0500

    Add partytown integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,19 +4,23 @@ import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import partytown from "@astrojs/partytown";
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = dirname(__filename);
+
+
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://www.brettjankord.com',
-	integrations: [image(), mdx(), sitemap()],
+  site: 'https://www.brettjankord.com',
+  integrations: [image(), mdx(), sitemap(), partytown()],
   vite: {
     resolve: {
-        alias: {
-          '$': path.resolve(__dirname, './src'),
-        },
-    },
+      alias: {
+        '$': path.resolve(__dirname, './src')
+      }
+    }
   }
 });
