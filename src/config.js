6e9e168e29c0ba924ced4b51138df4d0b7be27df
Brett <bjankord@gmail.com>
2022-10-19 05:58:07 -0500

    Inline homepage styles and update site title and description

--- src/config.js
+++ src/config.js
@@ -1,13 +1,10 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Brett Jankord';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
-
 export const SITE = {
     name: 'Brett Jankord',
     title: 'Brett Jankord',
-    description: 'Welcome to my website!',
+    description: 'Personal site for Brett Jankord, a Software Engineer living in the midwest who enjoys working on front-end development, design systems, accessibility, responsive web design, CSS, ReactJS / Vue.js, and open source software.',
     url: 'https://www.brettjankord.com',
     twitterUrl: 'https://twitter.com/bjankord',
     githubUrl: 'https://github.com/bjankord',
